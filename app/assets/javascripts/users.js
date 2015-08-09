598818446c43fc3f27b1057f32243b0df929a491
James Ferguson <jimmy2289@gmail.com>
2015-08-09 21:47:37 +0900

    Possible typos in js file.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
-  // Watch for a form submission
-  $("#form-submit-btn").click(function(event) {
+  // Watch for a form submission:
+  $('#form-submit-btn').click(function(event){
     event.preventDefault();
     $('input[type=submit]').prop('disabled', true);
     var error = false;
@@ -9,9 +9,9 @@ $(document).ready(function() {
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-
+    
     if (!error) {
-      // Get the stripe token:
+      // Get the Stripe token:
       Stripe.createToken({
         number: ccNum,
         cvc: cvcNum,
@@ -21,18 +21,18 @@ $(document).ready(function() {
     }
     return false;
   }); // form submission
-
+  
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
-    var f = $("#new-user");
-
+    var f = $('#new_user');
+    
     // Get the token from the response:
     var token = response.id;
-
+    
     // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-
-    // Submit the form
+    
+    // Submit the form:
     f.get(0).submit();
   }
 });
